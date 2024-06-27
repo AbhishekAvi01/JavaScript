b83b7c549ab65bd96437cf397fb24612ce45d533
AbhishekAvi01 <148855924+AbhishekAvi01@users.noreply.github.com>
2024-06-27 10:33:25 +0000

    datatype

---
+++ Basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3)// we are using node js , not browser
+
+console.log(3 
+    +3 ) // code readabilty should be high
+console.log("Abhishek");
+
+let name ="Abhishek" // string datatype
+let age =18 // number datatype
+let isLoggedIn = false // bolean dataType
+let state;
+
+// premetive dataType are following
+// number => 2 to power 53
+// bigint 
+// string => " "
+//boolean => true/ false
+// null  => standalone value => represention of empty value
+// undefine => value is not define 
+// symbol => using  unique  identify
+
+
+// object 
+console.log(typeof "Abhishek"); // string
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
+
